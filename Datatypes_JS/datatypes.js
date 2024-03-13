e38d367021651114e158edfda01fcef73089fef9
gayupuduvai <gayupuduvai@gmail.com>
2024-03-13 17:15:10 +0530

    wednesday

---
+++ Datatypes_JS/datatypes.js
@@ -0,0 +1,16 @@
+var name = "Priya";
+document.write("Priya")
+console.log(name, "name user");
+var name1; //variable declaration
+console.log(name1, "name1 user");
+name1 = "Ramu"; //variable definition
+var student_data = {
+    RollNo: "11TC2416",
+    Name: "Latha",
+    Age: 18,
+    Rank: 6
+}
+console.log(student_data, "student data");
+var course_array = ["HTML", "CSS"];
+console.log(course_array, "Course Array");
+var active = true;
