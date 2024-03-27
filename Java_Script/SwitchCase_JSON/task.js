206e4847f97122a46894ec6005c245cae0831af6
Vikas <rmjvv31@gmail.com>
2024-03-27 17:04:47 +0530

    27march

--- Java_Script/SwitchCase_JSON/task.js
+++ Java_Script/SwitchCase_JSON/task.js
@@ -1,1 +1,56 @@
-// Check 
+// Check Month number and print out its Name
+var month = parseInt(prompt("Enter the month:"));
+switch (month) {
+    case 1:
+        console.log(month, "January Month")
+        break;
+    case 2:
+        console.log(month, "February Month")
+        break;
+    case 3:
+        console.log(month, "March Month")
+        break;
+    case 4:
+        console.log(month, "April Month")
+        break;
+    case 5:
+        console.log(month, "May Month")
+        break;
+    case 6:
+        console.log(month, "June Month")
+        break;
+    case 7:
+        console.log(month, "July Month")
+        break;
+    case 8:
+        console.log(month, "August Month")
+        break;
+    case 9:
+        console.log(month, "September Month")
+        break;
+    case 10:
+        console.log(month, "October Month")
+        break;
+    case 11:
+        console.log(month, "November Month")
+        break;
+    case 12:
+        console.log(month, "December Month")
+        break;
+    default:
+        console.log(month, "Invalid data")
+}
+
+// Determine the letter is vowel or consonant:
+let letter = prompt("Please enter the letter:");
+switch (letter) {
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        console.log(letter, "is an Vowel");
+        break;
+    default:
+        console.log(letter, "is an Consonant");
+}
